eea54c1e66d4b8a58dc7692519c1ffc0a3e52bce
Uyen Tran <utra4729@uni.sydney.edu.au>
2023-02-08 13:25:41 +1100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
+;
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { IntroComponent } from './intro/intro.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorkComponent,
+    ContactComponent,
+    IntroComponent,
+    MenuComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/constants.ts
@@ -0,0 +1,31 @@
+export const email = 'uyen.trannn98@gmail.com';
+
+export const socialMedia = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/bchiang7',
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/bchiang7',
+  },
+];
+
+export const navLinks = [
+  {
+    name: 'About',
+    url: '/#about',
+  },
+  {
+    name: 'Experience',
+    url: '/#jobs',
+  },
+  {
+    name: 'Work',
+    url: '/#projects',
+  },
+  {
+    name: 'Contact',
+    url: '/#contact',
+  },
+];
